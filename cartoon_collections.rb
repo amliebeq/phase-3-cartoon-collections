bb073752cff8ec282eafb531cdbc34195128b07e
alex lieberman <alex.lieberman182@gmail.com>
2023-03-30 13:53:45 -0700

    commit tree f7932327539e406c7682690921914724a48e249d

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,19 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(arr)
+  arr.each.with_index(1) do |dwarf, index|
+    puts "#{index}. #{dwarf}"
+  end  
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arr)
+  arr.map { |element| "#{element.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  arr.filter { |call| call.length > 4}.length > 0
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  arr.find { |cheese| cheese_types.include?(cheese)}
+  
 end
